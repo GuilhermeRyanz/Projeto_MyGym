b149e7091f221614d9d09ec19d2c986be45fdc43
guilherme Ryan <guilherme.ryan.etech@gmail.com>
2024-12-16 14:54:06 -0300

    alteracoes nas mensagens de help

--- my-gym-frontend/src/app/modules/dashboards/components/plans-with-new-members-component/help-mensage/help-mensage.component.ts
+++ my-gym-frontend/src/app/modules/dashboards/components/plans-with-new-members-component/help-mensage/help-mensage.component.ts
@@ -13,7 +13,7 @@ import {MatButton} from "@angular/material/button";
     MatDialogClose
   ],
   template:`
-    <h1 mat-dialog-title>Grafico planos com novos alunos</h1>
+    <h1 mat-dialog-title>Gráfico de planos com novos alunos</h1>
     <div mat-dialog-content>Esse grafico mostra os planos em que houve um aumento no numeros de alunos nos ultimos 30 dias.</div>
     <div mat-dialog-actions>
     </div>
