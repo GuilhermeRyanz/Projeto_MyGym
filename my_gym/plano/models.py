a6561590e0de462acc89f3c4880677009fbf0f1a
Guilherme <guilherme.ryan.etech@gmail.com>
2025-03-04 21:34:33 -0300

    Update plans

--- my_gym/plano/models.py
+++ my_gym/plano/models.py
@@ -2,6 +2,52 @@ from django.db import models
 from academia.models import Academia
 from core.models import ModelBase
 
+from django.db import models
+from academia.models import Academia
+from core.models import ModelBase
+
+
+class DiasSemana(models.TextChoices):
+    SEGUNDA = "segunda", "Segunda-feira"
+    TERCA = "terca", "Terça-feira"
+    QUARTA = "quarta", "Quarta-feira"
+    QUINTA = "quinta", "Quinta-feira"
+    SEXTA = "sexta", "Sexta-feira"
+    SABADO = "sabado", "Sábado"
+    DOMINGO = "domingo", "Domingo"
+
+
+class TipoBeneficio(models.TextChoices):
+    DESCONTO = "desconto", "Desconto em produtos"
+    OUTROS = "outros", "Outros"
+    NENHUM = "nenhum", "Nenhum"
+
+
+class Beneficio(ModelBase, models.Model):
+    tipo_beneficio = models.CharField(
+        choices=TipoBeneficio.choices,
+        default=TipoBeneficio.NENHUM
+    )
+
+    descricao = models.CharField(
+        blank=True,
+        null=True,
+        help_text="Descricao detalhada dobeneficio",
+    )
+
+    desconto_percentual = models.DecimalField(
+        max_digits=5, decimal_places=2,
+        blank=True, null=True,
+        help_text="Percentual de desconto",
+    )
+
+    class Meta:
+        db_table = "beneficio"
+
+    def __str__(self):
+        return f"{self.tipo_beneficio} - {self.descricao or 'Sem descrição'}"
+
+
 class Plano(ModelBase):
     nome = models.CharField(
         db_column='nome',
@@ -25,11 +71,29 @@ class Plano(ModelBase):
         db_column='duracao'
     )
 
+    beneficios = models.ManyToManyField(
+        Beneficio,
+        related_name='planos',
+        blank=True,
+    )
+
     academia = models.ForeignKey(
         Academia,
         on_delete=models.CASCADE,
         db_column='academia',
         related_name='planos'
+    )
+
+    dias_permitidos = models.JSONField(
+        default=[DiasSemana.SEGUNDA,
+                 DiasSemana.TERCA,
+                 DiasSemana.QUARTA,
+                 DiasSemana.QUINTA,
+                 DiasSemana.SEXTA,
+                 DiasSemana.SABADO,
+                 DiasSemana.DOMINGO],
+        db_column="dias_permitidos",
+        help_text="Lista de dias da semana em que o plano pode ser usado"
     )
 
     class Meta:
--- my_gym/plano/serializers.py
+++ my_gym/plano/serializers.py
@@ -1,35 +1,83 @@
+from email.policy import default
+
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from academia.models import Academia
 from aluno.models import AlunoPlano
-from plano.models import Plano
+from plano.models import Plano, TipoBeneficio, Beneficio, DiasSemana
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from academia.models import Academia
+from aluno.models import AlunoPlano
+from plano.models import Plano, TipoBeneficio, Beneficio
+
+
+class BeneficioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Beneficio
+        fields = ['id', 'tipo_beneficio', 'descricao', 'desconto_percentual']
+
+    def validate(self, data):
+        tipo_beneficio = data.get('tipo_beneficio')
+        desconto_percentual = data.get('desconto_percentual')
+
+        if tipo_beneficio == TipoBeneficio.DESCONTO:
+            if desconto_percentual is None:
+                raise ValidationError("O desconto percentual é obrigatório quando o tipo de benefício é 'Desconto'.")
+            if desconto_percentual < 0 or desconto_percentual > 100:
+                raise ValidationError("O desconto percentual deve ser entre 0 e 100.")
+
+        return data
 
 
 class PlanoSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     nome = serializers.CharField(max_length=100)
     preco = serializers.DecimalField(max_digits=10, decimal_places=2)
-    descricao = serializers.CharField(default="Acesso total áacademia por um mes")
+    descricao = serializers.CharField(default="Acesso total à academia por um mês")
     duracao = serializers.IntegerField(default=1)
     academia = serializers.PrimaryKeyRelatedField(queryset=Academia.objects.all())
+    dias_permitidos = serializers.ListField(
+        child=serializers.ChoiceField(choices=DiasSemana.choices),
+        required=False
+    )
+    beneficios = BeneficioSerializer(many=True, required=False)
 
     class Meta:
         model = Plano
-        fields = ['active', 'id', 'nome', 'preco', 'descricao', 'duracao', 'academia']
+        fields = ['id', 'nome', 'preco', 'descricao', 'duracao', 'beneficios', 'academia', 'dias_permitidos']
 
     def validate_user_permission(self):
         request = self.context.get('request')
         if request.user.usuario.tipo_usuario == "A":
-            raise ValidationError("Tipo de usuario invalido")
+            raise ValidationError("Tipo de usuário inválido")
 
     def create(self, validated_data):
         self.validate_user_permission()
-        return super().create(validated_data)
+
+        beneficios_data = validated_data.pop('beneficios', [])
+        plano = Plano.objects.create(**validated_data)
+
+        for beneficio_data in beneficios_data:
+            beneficio = Beneficio.objects.create(**beneficio_data)
+            plano.beneficios.add(beneficio)
+
+        return plano
 
     def update(self, instance, validated_data):
-        self.validate_user_permission()
-        return super().update(instance, validated_data)
+        beneficios_data = validated_data.pop('beneficios', [])
+        instance = super().update(instance, validated_data)
+
+        if beneficios_data:
+            instance.beneficios.clear()
+            for beneficio_data in beneficios_data:
+                beneficio = Beneficio.objects.create(**beneficio_data)
+                instance.beneficios.add(beneficio)
+
+        return instance
 
 
 class PlanosAlunosAtivosSerializer(serializers.ModelSerializer):
@@ -38,7 +86,6 @@ class PlanosAlunosAtivosSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate(self, data):
-
         request = self.context.get('request')
 
         if request and request.user.usuario.tipo_usuario not in ["G", "D"]:
