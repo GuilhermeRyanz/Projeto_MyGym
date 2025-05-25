b99cc86859795c7320268d5ea14c0e8c9500dca7
Guilherme Ryan <guilherme.ryan.etech@gmail.com>
2025-05-25 15:13:31 -0300

    wip ia chat

--- my_gym/main.py
+++
@@ -1,4 +0,0 @@
-from chat_gym.chat import Chat
-quest = Chat("Voce e um contador de historias de fantasia e precisa saber informacoes sobre os diferentes contos literarios ja produzidos","Qual o nome do gato risonho do conto de alice no pais das maravilhas?" )
-
-print (quest.ask_question())
