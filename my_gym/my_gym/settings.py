369ba50402172336e8df1275c40e20c8aceb975a
Guilherme Ryan <guilherme.ryan.etech@gmail.com>
2025-05-31 22:24:34 -0300

    add celery worker

--- my_gym/my_gym/settings.py
+++ my_gym/my_gym/settings.py
@@ -200,4 +200,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = 'guilhermeryanfsantos@gmail.com'
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
